297ac25c1c465f88f45814c09f58ebf56e743f5c
= <=>
2026-03-17 14:52:49 +0800

    chore: SETUP husky

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,13 +8,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, world!');
+   res.send('Hello, world!');
 });
 
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({ message: 'Hello, world!' });
+   res.json({ message: 'Hello, world!' });
 });
 
 app.listen(port, () => {
-    console.log(`App is listening to port ${port}`);
-})
+   console.log(`App is listening to port ${port}`);
+});
