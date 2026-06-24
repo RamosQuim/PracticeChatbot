6111a32cc0c0d062c5b669eeca8ef4b1339798c3
RamosQuim <qlramos@up.edu.ph>
2026-06-24 18:54:42 +0800

    refactor: REFACTORED index.ts

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,50 +1,15 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import z from 'zod';
-import { chatService } from './services/chat.service';
+import router from './routes';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // middleware function for parsing data
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello, world!');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello, world!' });
-});
-
-const chatSchema = z.object({
-   prompt: z
-      .string()
-      .trim()
-      .min(1, 'Prompt is required.')
-      .max(1000, 'Prompt is too long (max: 1000 characters)'),
-   conversationId: z.uuid(),
-});
-
-app.post('/api/chat', async (req: Request, res: Response) => {
-   const parseResult = chatSchema.safeParse(req.body);
-
-   if (!parseResult.success) {
-      res.status(400).json(z.treeifyError(parseResult.error));
-      return;
-   }
-
-   try {
-      const { prompt, conversationId } = req.body;
-      const response = await chatService.sendMessage(prompt, conversationId);
-
-      res.json({ message: response.message });
-   } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Something went wrong.' });
-   }
-});
 
 app.listen(port, () => {
    console.log(`App is listening to port ${port}`);
