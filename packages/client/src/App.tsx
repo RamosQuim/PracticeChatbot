a983441e3a7a28b449ad6a8067bfe50fa20e114a
RamosQuim <qlramos@up.edu.ph>
2026-06-26 21:54:54 +0800

    refactor: REFACTORED code structure of chatbot

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,9 @@
 import { useState } from 'react';
-import ChatBot from './components/ChatBot';
+import ChatBot from './components/chat/ChatBot';
 
 function App() {
    return (
-      <div className="flex flex-col justify-center items-stretch p-8 gap-8 h-screen">
-         <h1 className="text-center text-3xl font-bold">
-            Welcome to our ChatBot!
-         </h1>
+      <div className="p-4 h-screen w-full">
          <ChatBot />
       </div>
    );
