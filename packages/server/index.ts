1a6c332648dfbe55570e8475d52fba834f7cb75a
RamosQuim <qlramos@up.edu.ph>
2026-07-04 19:29:10 +0800

    feat: SETUP product review summarizer schemas

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,12 +2,19 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import router from './routes';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from './generated/prisma/client';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // middleware function for parsing data
 app.use(router);
+
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+export const prisma = new PrismaClient({
+   adapter,
+});
 
 const port = process.env.PORT || 3000;
 
