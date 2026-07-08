6642de27a02121f7de77247458bfa48b4b1a830c
RamosQuim <qlramos@up.edu.ph>
2026-07-08 20:22:56 +0800

    feat: SETUP LLM for review summarizer

--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,5 +1,11 @@
+import { CohereClientV2 } from 'cohere-ai';
 import type { Review } from '../generated/prisma/browser';
 import { reviewRepository } from '../repositories/review.repository';
+
+// Implementation detail
+const client = new CohereClientV2({
+   token: process.env.LLM_API_KEY,
+});
 
 export const reviewService = {
    async getReviews(productId: number): Promise<Review[]> {
@@ -11,9 +17,31 @@ export const reviewService = {
       // send reviews to an LLM
       const reviews = await reviewRepository.getReviews(product, 10);
       const joinedReviews = reviews.map((r) => r.content).join('\n\n');
+      const prompt = [
+         {
+            role: 'user',
+            content: `
+        Summarize the following customer reviews into a short paragraph
+        highlighting key themes, both positive and negative:
 
-      const summary = 'This is a placeholder summary.';
+        ${joinedReviews}`,
+         },
+      ];
 
-      return summary;
+      const response = await client.chat({
+         model: 'command-a-03-2025',
+         temperature: 0.2,
+         maxTokens: 500,
+         messages: prompt,
+      });
+
+      // correctly parse the response object
+      const reply =
+         response.message?.content
+            ?.filter((item) => item.type === 'text')
+            ?.map((item) => item.text)
+            ?.join('') || '';
+
+      return reply;
    },
 };
